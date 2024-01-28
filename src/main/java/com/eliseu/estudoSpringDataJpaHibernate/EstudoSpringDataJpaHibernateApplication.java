96a407b2984d9f4658600d111b370f723c508e78
eliseusbrito <eliseusbrito@gmail.com>
2024-01-28 09:23:33 -0300

    Projeto criado com spring start io

---
+++ src/main/java/com/eliseu/estudoSpringDataJpaHibernate/EstudoSpringDataJpaHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.eliseu.estudoSpringDataJpaHibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudoSpringDataJpaHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudoSpringDataJpaHibernateApplication.class, args);
+	}
+
+}
